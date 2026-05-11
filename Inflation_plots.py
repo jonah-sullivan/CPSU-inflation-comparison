45a46743dea724de9511d235b397e9cce1db50b5
jonah <jonahsullivan79@gmail.com>
2026-05-12 09:26:27 +1000

    Fix subplot x-axis labels and legend handling

--- Inflation_plots.py
+++ Inflation_plots.py
@@ -2,10 +2,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
+import matplotlib.dates as mdates
 
 # Data
 pay_classes = ["APS4.4", "APS5.4", "APS6.5", "EL1.5", "EL2.6"]
 df = pd.read_excel('inflation_tables.xlsx', index_col='Pay Date')
+df.index = pd.to_datetime(df.index)
 index_dict = {'SLCI': 'Monthly SLCI', 'CPI': 'Monthly CPI'}
 
 
@@ -34,25 +36,44 @@ for inflation_index in index_dict.keys():
         df[f"{pay_level} cumulative difference"] = df[f"{pay_level} Difference"].cumsum()
 
     # Plot
-    fig, axes = plt.subplots(3,2, figsize=(14,13), sharex=True, layout='constrained')
+    fig, axes = plt.subplots(3, 2, figsize=(14,13), layout='constrained')
+
+    xmin = df.index.min()
+    xmax = df.index.max()
+
+    for a in axes.ravel():
+        a.set_xlim(xmin, xmax)
 
     for pay_level, ax in zip(pay_classes, axes.ravel()):
-        df[f"{pay_level} Monthly Pay with " + inflation_index].plot(ax=ax)
-        df[f"{pay_level} Monthly Pay"].plot(ax=ax)
+        df[f"{pay_level} Monthly Pay with " + inflation_index].plot(
+            ax=ax, label=f"Inflation adjusted ({inflation_index})"
+        )
+        df[f"{pay_level} Monthly Pay"].plot(
+            ax=ax, label="Actual pay"
+        )
+
+        years = pd.date_range(xmin, xmax, freq='YS')
+        ax.set_xticks(years)
+        ax.set_xticklabels([y.year for y in years])
+        ax.set_xlabel('Year')
+
         ax.yaxis.set_major_formatter(mtick.StrMethodFormatter('${x:,.0f}'))
         ax.set_ylabel('Gross Monthly Pay ($)')
-        ax.set_xlabel(None)
         ax.grid(alpha=0.5)
         ax.set_title(pay_level, fontsize=14)
-        ax.legend(loc='upper left')
-        ax.text(.02, .8, 
-                'Total Cumulative Difference=${:,.0f}'.
-                format(df[f"{pay_level} cumulative difference"].iloc[-1]),
-                transform=ax.transAxes, fontsize=11)
 
-    # Turn off frame for last subplot
-    axes[2,1].axis("off");
+        ax.legend(loc='upper left', frameon=False, fontsize=10)
 
+        
+        ax.text(
+                .02, .8,
+                'Total Cumulative Difference=${:,.0f}'.format(
+                    df[f"{pay_level} cumulative difference"].iloc[-1]
+                ),
+                transform=ax.transAxes, fontsize=11
+            )
+
+    fig.delaxes(axes[2, 1])
     fig.savefig(f'inflation_adjusted_pay_comparison_' + inflation_index + '.png', bbox_inches='tight', dpi=300)
 
 
